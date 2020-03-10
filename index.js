a1bbee6017cb98271b40d8310a5f07e478238013
abishekravi <abishekravi@yahoo.com>
2020-03-10 14:59:40 +0530

    Update index.js
    added back the other login options

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ var uiConfig = {
     //signInSuccessUrl: 'https://teachablemachine.withgoogle.com/models/aqeZ_cfa/',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
-      //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+      firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
     tosUrl: 'main.html',
